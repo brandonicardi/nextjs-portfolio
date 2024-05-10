57a98d57df1b3c889af3dc432203124779765b81
brandonicardi <brandonicardi@gmail.com>
2024-05-10 19:37:03 -0300

    Modificacion en data.ts

--- lib/data.ts
+++ lib/data.ts
@@ -33,20 +33,12 @@ export const links = [
 
 export const experiencesData = [
   {
-    title: "Servicio Tecnico en Computación",
-    location: "RISUS Computación",
+    title: "Analista Programador Back-End",
+    location: "SISEM",
     description:
-      "Me especialicé en el formateo e instalación de programas, la recuperación de datos y la eliminación de virus. Además, tengo experiencia en el mantenimiento de hardware, incluido el ensamblaje de computadoras, así como en el diagnóstico y la resolución de errores técnicos.",
+      "Me he destacado por adaptarme y aprender sobre herramientas como JxNet/.NET. Participo activamente en reuniones con clientes y logre hacerme de experiencia en el manejo y creación de consultas (queries), procedimientos almacenados (sp's) y manipulación de datos (DML's) utilizando T-SQL.",
     icon: React.createElement(CgWorkAlt),
-    date: "2019 - 2021",
-  },
-  {
-    title: "Licenciatura en Sistemas",
-    location: "Universidad Nacional de Lanús",
-    description:
-    "Actualmente estoy en el tercer año de mi carrera, con un promedio general de 8. A lo largo de mi trayectoria académica, he llevado a cabo proyectos que me han permitido adquirir experiencia en desarrollo utilizando tecnologías clave como Java.",
-    icon: React.createElement(LuGraduationCap),
-    date: "2021",
+    date: "2023 - Actualidad",
   },
   {
     title: "Desarrollo en Aplicaciones Web",
@@ -57,12 +49,20 @@ export const experiencesData = [
     date: "2021 - 2022",
   },
   {
-    title: "Analista Programador Back-End",
-    location: "SISEM",
+    title: "Licenciatura en Sistemas",
+    location: "Universidad Nacional de Lanús",
     description:
-      "Me he destacado por adaptarme y aprender sobre herramientas como JxNet/.NET. Participo activamente en reuniones con clientes y logre hacerme de experiencia en el manejo y creación de consultas (queries), procedimientos almacenados (sp's) y manipulación de datos (DML's) utilizando T-SQL.",
+      "Actualmente estoy en el tercer año de mi carrera, con un promedio general de 8. A lo largo de mi trayectoria académica, he llevado a cabo proyectos que me han permitido adquirir experiencia en desarrollo utilizando tecnologías clave como Java.",
+    icon: React.createElement(LuGraduationCap),
+    date: "2021",
+  },
+  {
+    title: "Servicio Tecnico en Computación",
+    location: "RISUS Computación",
+    description:
+      "Me especialicé en el formateo e instalación de programas, la recuperación de datos y la eliminación de virus. Además, tengo experiencia en el mantenimiento de hardware, incluido el ensamblaje de computadoras, así como en el diagnóstico y la resolución de errores técnicos.",
     icon: React.createElement(CgWorkAlt),
-    date: "2023 - Actualidad",
+    date: "2019 - 2021",
   },
 ] as const;
 
