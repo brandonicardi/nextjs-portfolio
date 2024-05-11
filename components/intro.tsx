90fb3b63a825ad37c756ce2935b6725aeba328d8
brandonicardi <brandonicardi@gmail.com>
2024-05-10 21:20:42 -0300

    Cambios en texto.

--- components/intro.tsx
+++ components/intro.tsx
@@ -65,7 +65,7 @@ export default function Intro() {
         <span className="font-bold">Hola, mi nombre es Brandon.</span> Soy un{" "}
         <span className="font-bold">programador full-stack junior</span> con{" "}
         <span className="font-bold">1 año de experiencia</span>. Me encuentro en busca de mi proxima{" "}
-        <span className="underline">Oportunidad laboral</span>.
+        <span className="font-bold">oportunidad laboral</span>.
       </motion.h1>
 
       <motion.div
